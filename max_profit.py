407e49fe1213c361db0fb7e5e2f4da3c425663a2
안재훈 <dkswogns1132@naver.com>
2023-09-14 11:28:52 +0900

    Update max_profit.py

--- max_profit.py
+++ max_profit.py
@@ -1,9 +1,9 @@
 def maxProfit_bruteforce (prices):
    max_price = 0
 
-   for i, price in enumerate(prices):
-       for j in range(i, len(prices)):
-           max_price = max(prices[j] - price, max_price)
+   for i, price in enumerate(price):
+       for j in range(i, len(price)):
+           max_price = max(price[j] - price, max_price)
 
    return
 
