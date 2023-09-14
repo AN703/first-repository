02197af32216091b5a6a2a00162b412f519c2959
안재훈 <dkswogns1132@naver.com>
2023-09-14 11:31:42 +0900

    re:

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     pn.is_prime_number(24)
     pn.is_prime_number(31)
     pn.is_prime_number(1651651)
-    pn.is_prime_number(238674237)
+    pn.is_prime_number(23862374237)
